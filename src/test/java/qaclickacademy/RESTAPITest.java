8de78079ab01f825a8e716c163bc72b267de45d3
teju <tejuanil2010@gmail.com>
2021-07-29 23:09:23 +1000

    fourth commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -13,6 +13,9 @@ public class RESTAPITest {
 	System.out.println("postJira3");
 	System.out.println("postJira4");
 	System.out.println("postJira5");
+	System.out.println("postJira6");
+	System.out.println("postJira7");
+	System.out.println("postJira8");
 	}
 	
 	@Test
